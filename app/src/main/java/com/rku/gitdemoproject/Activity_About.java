090560e2151a45cacea04eb1babdb38d9e6ac1c6
ssingh <ssingh862@rku.ac.in>
2020-07-31 22:21:50 +0530

    First Comment

---
+++ app/src/main/java/com/rku/gitdemoproject/Activity_About.java
@@ -0,0 +1,51 @@
+package com.rku.gitdemoproject;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class Activity_About extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.custom_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.menuSplash:
+                Intent intentSplash = new Intent(Activity_About.this,Activity_Splash.class);
+                startActivity(intentSplash);
+                Toast.makeText(this, "Splash Clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menuLogin:
+                Intent intentLogin = new Intent(Activity_About.this,Activity_Login.class);
+                startActivity(intentLogin);
+                Toast.makeText(this, "Login Clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menuAbout:
+                Toast.makeText(this, "About Clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.menuContact:
+                Intent intentContact = new Intent(Activity_About.this,Activity_Contact.class);
+                startActivity(intentContact);
+                Toast.makeText(this, "Contact Clicked", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
